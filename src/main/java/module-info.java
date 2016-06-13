4c2c75f6952cf020096e83917ad86ca28eabad6d
Gunnar Morling <gunnar.morling@googlemail.com>
2016-06-13 17:32:55 +0200

    [jigsaw] Adding module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module javax.validation {
+    exports javax.validation;
+    exports javax.validation.bootstrap;
+    exports javax.validation.constraints;
+    exports javax.validation.constraintvalidation;
+    exports javax.validation.executable;
+    exports javax.validation.groups;
+    exports javax.validation.metadata;
+    exports javax.validation.spi;
+    uses javax.validation.spi.ValidationProvider;
+}
